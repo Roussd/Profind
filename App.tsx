5c33ddcba3f44ad4088ad53e3bececb5395ece17
Roussd <aracelli.rivas2101@alumnos.ubiobio.cl>
2025-02-06 10:49:58 -0300

    Add ThemeContext and integrate theme provider in the app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import { ExpoRoot } from "expo-router";
-import { RegisterProvider } from "./context/userRegisterContext"; // Importa el contexto
+import { RegisterProvider } from "./context/userRegisterContext";
+import { ThemeProvider } from "./context/ThemeContext"; 
 
 export default function App() {
+  // @ts-ignore
   const ctx = require.context("./app");
   return (
     <RegisterProvider>
-      <ExpoRoot context={ctx} />
+      <ThemeProvider>
+        <ExpoRoot context={ctx} />
+      </ThemeProvider>
     </RegisterProvider>
   );
 }
+
+// se ignora el error de context, ya que no se puede importar el contexto de la forma tradicional
+// este error aparece porque require.context no forma parte de la API estándar de node ni de typeScript
